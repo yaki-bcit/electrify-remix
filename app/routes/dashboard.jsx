cb81f07eb86033bab1733ad968a4bd92499f1129
Yevgeniy Akimenko <yakimenko@bcit.ca>
2022-11-27 14:22:39 -0800

    more in Dashboard

--- app/routes/dashboard.jsx
+++ app/routes/dashboard.jsx
@@ -1,12 +1,20 @@
 import PageHeading from "~/components/PageHeading";
 import WhiteBox from "~/components/WhiteBox";
 import Table from "~/components/Table";
+import Paragraph from "~/components/Paragraph";
 
 export default function Dashboard() {
   return (
     <>
     <PageHeading text="Dashboard" />
     <WhiteBox>
+      <h2 className="text-lg font-semibold">
+        EV Advice for You
+      </h2>
+      <Paragraph text="You should buy a Tesla Model 3." />
+      <h2 className="text-lg font-semibold mt-4">
+        Knowledge Base and Quizzes
+      </h2>
       <Table />
     </WhiteBox>
     </>
