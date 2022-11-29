9e1601d1cdd8a9c447a8ed574f491302546ea5ef
Kevin Hung <kevincjhung@gmail.com>
2022-11-28 18:06:48 -0800

    landingPage styling progress

--- app/routes/landingPage.jsx
+++ app/routes/landingPage.jsx
@@ -3,21 +3,27 @@ import ButtonDark from "~/components/ButtonDark";
 import ButtonLight from "~/components/ButtonLight";
 
 export default function LandingPage() {
-    let text = `
+	let text = `
         Are you planning on getting an electric car?
         This app will help you figure out if it is the right decision for you.
     `;
 
 
-    return (
-        <main>
-            <h1>Posts</h1>
-            <img src="/images/Logo.svg"/>
-            <div className="bg-white">
-                <Paragraph text={text} />
-                <ButtonDark text="Get Started" />
-                <ButtonLight text="Do I Need An Electric Vehicle?" />
-            </div>
-        </main>
-    )
+	return (
+		<main>
+			<img src="/images/Logo.svg" className="" />
+			<div className="bg-white m-6 p-6 flex flex-col">
+				<Paragraph text={text} />
+				<div className="">
+					<div className="m-3 ">
+						<ButtonDark text="Get Started" className=""/>
+					</div>
+					<div className="m-3">
+						<ButtonLight className="p-4 m-4" text="Do I Need An Electric Vehicle?" />
+					</div>
+				</div>
+
+			</div>
+		</main>
+	)
 }
