703c9d840600a8fff772783fad2c00aac3b7c919
Kevin Hung <kevincjhung@gmail.com>
2022-11-29 16:24:38 -0800

    congratsPage and testPage styling

--- app/routes/congratsPage.jsx
+++ app/routes/congratsPage.jsx
@@ -1,6 +1,32 @@
-export default function congratsPage(){
-    return (
-        <>
-        </>
-    )
+import ButtonDark from "~/components/ButtonDark"
+import ButtonLight from "~/components/ButtonLight"
+import Paragraph from "~/components/Paragraph"
+import { Link } from "@remix-run/react";
+
+export default function congratsPage() {
+	const text = `
+		Congratulations, you are the father!
+	`
+
+
+	return (
+		<>
+			<div className="block justify-items-center">
+				<img src="/images/Logo.svg" className="mx-auto" />
+			</div>
+			<div className="bg-white p-3 text-center">
+			<Paragraph text={text} className="p-4"/>		
+				<Link to="/">
+					<div className="p-4">
+						<ButtonDark text="Get Started" className=""/> 
+					</div>
+				</Link>
+			  <Link to="/"> 
+					<div className="p-3">
+						<ButtonLight text="Try The Quiz Again"/>
+					</div>
+				</Link>
+			</div>
+		</>
+	)
 }
--- app/routes/testPage.jsx
+++ app/routes/testPage.jsx
@@ -1,5 +1,7 @@
-import Paragraph from "~/components/Paragraph"
 
+import ButtonDark from "~/components/ButtonDark"
+import Paragraph from "~/components/Paragraph"
+import { Link } from "@remix-run/react";
 
 export default function testpage() {
 	let text = `
@@ -11,9 +13,21 @@ export default function testpage() {
 
 	return (
 		<>
-			<img src="/images/Logo.svg" className="" />
-			<div className="bg-white padding-3">
-				<Paragraph text={text} />		
+			<div className="block justify-items-center">	
+				<img src="/images/Logo.svg" className="mx-auto" />
+			</div>
+			<div className="bg-white p-3 text-center">
+				<Paragraph text={text} className="p-4"/>		
+				<Link to="/">
+					<div className="p-4">
+						<ButtonDark text="Start The Quiz" className=""/> 
+					</div>
+				</Link>
+			  <Link to="/"> 
+					<div className="p-3">
+						<ButtonDark text="Go Back"/>
+					</div>
+				</Link>
 			</div>
 		</>
 	)
