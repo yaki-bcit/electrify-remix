1604226ba8d40038eaa3c395dcc56d9f3b3c6f37
Cocoandjane <janeliangbc@gmail.com>
2022-12-08 14:48:48 -0800

    added opening quiz

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,15 +1,33 @@
 import {
   Outlet
 } from "@remix-run/react";
+import Paragraph from "~/components/Paragraph";
+import ButtonDark from "~/components/Buttons/ButtonDark";
+import ButtonLight from "~/components/Buttons/ButtonLight";
+
 
 import WhiteBox from "~/components/WhiteBox";
 
 // a loader to redirect the user to the dashboard if they have already done onboarding
 
 export default function Index() {
+  let text = `
+        Are you planning on getting an electric car?
+        This app will help you figure out if it is the right decision for you.
+    `;
+
   return (
     <div style={{ fontFamily: "Mina, system-ui, sans-serif", lineHeight: "1.4" }} className="">
-      
+      	<main>
+			<div className="block justify-items-center">
+				<img src="/images/Logo.svg" className="mx-auto" />
+			</div>
+			<div className="h-[400px] bg-white rounded-lg p-5 mb-4 text-lg  grid grid-rows-2 text-center">
+				<Paragraph text={text} />
+				<ButtonDark href="/home" text="Get Started" />
+				<ButtonLight href="/testPage" text="Do I need an EV" />
+			</div>
+		</main>
     </div>
   );
 }
--- app/utils/quizzes.js
+++ app/utils/quizzes.js
@@ -1,4 +1,180 @@
 export const quizzes = {
+  openning: {
+    title: 'Is an Electric Vehicle (EV)a good choice for you?',
+    questions: [
+      {
+        title: 'Question 1', // title can be more descriptive, like the topic of the question
+        text: 'what would you use your car for?',
+        multiple: true, // does the question have several correct answers? if yes, checkboxes will be used. if not, radio buttons will be used.
+        options: [
+          {
+            text: 'Work or school',
+            value: 'Work or school', // value is a shorter version of text if text is very long
+            correct: true,
+            checked: false,
+          },
+          {
+            text: 'Long distance travel',
+            value: 'Long distance travel',
+            correct: true,
+            checked: false,
+          },
+          {
+            text: 'Uber driving',
+            value: 'Uber driving',
+            correct: true,
+            checked: false
+          },
+          {
+            text:'Groceries and Activityes',
+            value: 'Groceries and Activityes',
+            correct: true,
+            checked: false,
+          },
+          {
+            text:'Others',
+            value: 'Others',
+            correct: true,
+            checked: false,
+          },
+        ],
+      },
+      {
+        title: 'Question 2',
+        text: 'Have you ever used a Electric Car?',
+        multiple: false,
+        options: [
+          {
+            text:'Yes',
+            value: 'Yes',
+            correct: true,
+            checked: false,
+          },
+          {
+            text: 'No',
+            value: 'No',
+            correct: true,
+            checked: false,
+          
+          },
+        ],
+      },
+      {
+        title: 'Question 3',
+        text: 'How many hours do you drive Weekly?',
+        multiple: false,
+        options: [
+          {
+            text: 'Less Than 5 hours',
+            value: 'Less Than 5 hours',
+            correct: false,
+            checked: false,
+          },
+          {
+            Text:'Around 10 hours',
+            value: 'Around 10 hours',
+            correct: false,
+            checked: false,
+          },
+          {
+            text: 'Around 15 hours',
+            value: 'Around 15 hours',
+            correct: false,
+            checked: false,
+          },
+          {
+            text: 'Around 20 hours',
+            value: 'Around 20 hours',
+            correct: true,
+            checked: false,
+          },
+          {
+            text: 'More thank 30 hours',
+            value: 'More thank 30 hours',
+            correct: true,
+            checked: false,
+          },
+
+        ],
+      },
+      {
+        title: 'Question 4',
+        text: 'What type of road do you drive you car?',
+        multiple: true,
+        options: [
+          {
+            text: 'Pavement',
+            value: 'Pavement',
+            correct: true,
+            checked: false,
+          },
+          {
+            text: 'Mud Road',
+            value: 'Mud Road',
+            correct: true,
+            checked: false,
+          },
+          {
+            text: 'Dirt Road',
+            value: 'Dirt Road',
+            correct: true,
+            checked: false,
+          },
+          {
+            text: 'Desert',
+            value: 'Desert',
+            correct: false,
+            checked: false,
+          },
+          {
+            text: 'Snowy road',
+            value: 'Snowy road',
+            correct: true,
+            checked: false,
+          },
+
+        ],
+      },
+      {
+        title: 'Question 4',
+        text: 'How many passengers do you usually take when you drive?',
+        multiple: true,
+        options: [
+          {
+            text: 'No passengers',
+            value: 'No passengers',
+            correct: true,
+            checked: false,
+          },
+          {
+            text: '1 -2  Passengers',
+            value: '1 -2  Passengers',
+            correct: true,
+            checked: false,
+          },
+          {
+            text: '3-4  Passengers',
+            value: '3-4  Passengers',
+            correct: true,
+            checked: false,
+          },
+          {
+            text: '5-6 Passengers',
+            value: '5-6 Passengers',
+            correct: false,
+            checked: false,
+          },
+          {
+            text: 'More than 6 Passengers',
+            value: 'More than 6 Passengers',
+            correct: true,
+            checked: false,
+          },
+
+        ],
+      },
+    ]
+  },
   charging: {
     title: 'EV Charging',
     questions: [
@@ -230,6 +406,4 @@ export const quizzes = {
       },
     ]
   },
-
-
 }
